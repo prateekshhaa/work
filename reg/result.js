843808109f6183bf439a72438fa63951a6c1c911
prateekshhaa <68272000+prateekshhaa@users.noreply.github.com>
2020-08-19 00:20:18 +0530

    Add files via upload

---
+++ reg/result.js
@@ -0,0 +1,24 @@
+
+    function validate()
+     {
+        var name=document.getElementById('name').value;
+        var dob=document.getElementById('dob').value
+        var email=document.getElementById('email').value
+        var phn=document.getElementById('phn').value
+        var bd=document.getElementById('bd').value
+
+        document.write("<h3> Your Details Are: </h3>");
+
+        document.write ("Name : " +  name + "<br/>");
+        document.write("DOB : " +  dob + "<br/>");
+        document.write("Email : "  + email + "<br/>");
+        document.write("Phone no : " +  phn + "<br/>");
+        document.write("Business Details : " +  bd + "<br/>");
+
+    }
+      
+    
+
+
+    
+    
